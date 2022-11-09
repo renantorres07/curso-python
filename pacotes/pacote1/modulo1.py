0a5440b174e06d66a607fbfbae03703bcadd1c71
unknown <nantorres0@gmail.com>
2022-11-09 17:24:55 -0300

    Curso Python

---
+++ pacotes/pacote1/modulo1.py
@@ -0,0 +1,4 @@
+print(f'importado módulo {__name__} do pacote {__package__}')
+
+def soma(x, y):
+    return x + y
---
+++ pacotes/pacote1/modulo2.py
@@ -0,0 +1,5 @@
+def main():
+    print(f'Rodando o main() no modulo {__name__}')
+
+if __name__ == '__main__':
+    main()
